4a2877787ec1aabcb269447e3f8a8da392130161
madhavi0891 <66946576+madhavi0891@users.noreply.github.com>
2020-12-17 16:03:49 +0530

    Update vars.tf

--- vars.tf
+++ vars.tf
@@ -2,16 +2,17 @@ variable "AWS_REGION" {
   default = "us-west-2"
 }
 variable "PATH_TO_PRIVATE_KEY" {
-  default = "oregon"
+  default = "mykey"
 }
 variable "PATH_TO_PUBLIC_KEY" {
-  default = "oregon.pub"
+  default = "mykey.pub"
 }
 variable "AMIS" {
-  type = "map"
   default = {
-    us-west-2 = "ami-0a243dbef00e96192"
-    us-west-2 = "ami-003634241a8fcdec0"
-    us-west-2 = "ami-0fb16d6843cf910d7"
+    us-west-2 = "ami-0ac73f33a1888c64a"
+
   }
+}
+variable "instance_count" {
+  default = "2"
 }
